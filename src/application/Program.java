a3f3ccb5e910907e9ca4f64dc41cd05dcc2a3bcd
unknown <jbrunorodrigues13@gmail.com>
2026-04-02 14:02:24 -0300

    Classe Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,18 @@
 package application;
 
+import java.util.Date;
+
 import model_entities.Department;
+import model_entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Department obj = new Department(1, "Test");
-		
 		System.out.println(obj);
+		
+		Seller seller = new Seller(2, "Fulano", "fulano@gmail.com", new Date(), 1000.0, obj);
+		System.out.println(seller);
 	}
-
 }
